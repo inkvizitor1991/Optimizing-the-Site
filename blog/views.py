37937f99e979cd1d5a7c615499d3377b7b0daf17
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-10-25 14:19:51 +0300

    optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -28,14 +38,18 @@ def serialize_tag(tag):
     }
 
 
-def get_likes_count(post):
-    return post.likes.count()
-
-
 def index(request):
     posts = Post.objects.prefetch_related('author')
     most_popular_posts = posts.annotate(likes_count=Count('likes')).order_by(
         '-likes_count')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = posts.filter(id__in=most_popular_posts_ids).annotate(
+        comments_count=Count('comments'))[:5]
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
     fresh_posts = posts.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
